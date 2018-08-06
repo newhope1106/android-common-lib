dbb558d285384c1f1c4ee89970c570b891a8036b
newhope1106 <408705420@qq.com>
2018-08-06 17:52:42 +0800

    添加demo界面代码

---
+++ app/src/main/java/cn/appleye/demo/DemoActivity.java
@@ -0,0 +1,33 @@
+package cn.appleye.demo;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import cn.appleye.demo.R;
+
+public class DemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+
+        findViewById(R.id.demo_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        findViewById(R.id.document_view).setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+}
