6e47b9665d9a9e15401dc904519b17f6e9c97629
newhope1106 <408705420@qq.com>
2018-08-10 15:08:34 +0800

    添加菜单布局，待完善

--- app/src/main/java/cn/appleye/demo/DemoShowActivity.java
+++ app/src/main/java/cn/appleye/demo/DemoShowActivity.java
@@ -2,9 +2,15 @@ package cn.appleye.demo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.yarolegovich.slidingrootnav.SlidingRootNav;
 import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
@@ -14,6 +20,8 @@ import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
  * */
 public class DemoShowActivity extends AppCompatActivity {
     private SlidingRootNav mSlidingRootNav;
+    private RecyclerView mRecyclerView;
+    private RecyclerViewAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +53,39 @@ public class DemoShowActivity extends AppCompatActivity {
                 startActivity(new Intent(DemoShowActivity.this, ReadMeActivity.class));
             }
         });
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setNestedScrollingEnabled(false);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new RecyclerViewAdapter();
+        mRecyclerView.setAdapter(mAdapter);
     }
 
+    private class RecyclerViewAdapter extends RecyclerView.Adapter<ViewHolder>{
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(DemoShowActivity.this).inflate(R.layout.menu_item, null);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            TextView textView = (TextView)holder.itemView;
+            textView.setText("position - " + (position + 1));
+        }
+
+        @Override
+        public int getItemCount() {
+            return 10;
+        }
+    }
+
+    private class ViewHolder extends RecyclerView.ViewHolder{
+
+        ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
