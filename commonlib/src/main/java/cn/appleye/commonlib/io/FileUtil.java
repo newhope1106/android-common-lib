748f8b1c58efc4ed230bc6093c727b52ba1f6c56
newhope1106 <408705420@qq.com>
2018-08-22 17:45:56 +0800

    添加压缩和文件处理工具类

---
+++ commonlib/src/main/java/cn/appleye/commonlib/io/FileUtil.java
@@ -0,0 +1,9 @@
+package cn.appleye.commonlib.io;
+
+/**
+ * @author newhope1106
+ * date 2018/8/22
+ * 文件相关
+ */
+public class FileUtil {
+}
---
+++ commonlib/src/main/java/cn/appleye/commonlib/io/ZipUtil.java
@@ -0,0 +1,10 @@
+package cn.appleye.commonlib.io;
+
+/**
+ * @author newhope1106
+ * date 2018/8/22
+ * 压缩和解压
+ */
+public class ZipUtil {
+
+}
